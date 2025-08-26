662f32b652bb5bab13bae11f7f86a7408bb4eedd
gboeer <1067159+gboeer@users.noreply.github.com>
2025-08-26 13:42:42 +0200

    ♻️ encapsulate functionality within HODOR_Dataset class

--- src/hodor_python/dataset.py
+++ src/hodor_python/dataset.py
@@ -1,23 +1,33 @@
 import requests
 import pandas as pd
-
-def download_tab_file(output_path: str):
-    url = "https://doi.pangaea.de/10.1594/PANGAEA.980059?format=textfile"
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an error for bad status codes
-    with open(output_path, 'wb') as f:
-        f.write(response.content)
+from pathlib import Path
 
 
+class HODOR_Dataset:
+    def __init__(self, filepath: str):
+        self.filepath = Path(filepath)
+        if not self.filepath.exists():
+            self.download_tab_file(self.filepath)
 
-def hodor_as_pandas(filepath):
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-    # Find end of header
-    for i, line in enumerate(lines):
-        if line.startswith('*/') and not line.strip() == '':
-            header_idx = i
-            break
-    # Read the data using pandas, skipping the header block
-    df = pd.read_csv(filepath, sep='\t', skiprows=header_idx)
-    return df
+        self.df = self._load_dataframe()
+        self._url = "https://doi.pangaea.de/10.1594/PANGAEA.980059?format=textfile"
+
+    def download_tab_file(self, output_path: str):
+        response = requests.get(self._url)
+        response.raise_for_status()
+        with open(output_path, "wb") as f:
+            f.write(response.content)
+
+    def _load_dataframe(self):
+        with open(self.filepath, "r") as f:
+            lines = f.readlines()
+        # Find end of header
+        for i, line in enumerate(lines):
+            if line.startswith("*/") and not line.strip() == "":
+                header_idx = i
+                break
+        # Read the data using pandas, skipping the header block
+        df = pd.read_csv(self.filepath, sep="\t", header=1, skiprows=header_idx)
+        df["Date/time start"] = pd.to_datetime(df["Date/time start"])
+        df["Date/time end"] = pd.to_datetime(df["Date/time end"])
+        return df
